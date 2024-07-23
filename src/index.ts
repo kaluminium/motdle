ba41370baeca704b30785fc693c25d627e86edc8
Luka Lindberg <48952912+kaluminium@users.noreply.github.com>
2024-07-23 16:43:06 +0200

    emojis testing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, Message } from 'discord.js';
 import dotenv from 'dotenv';
+const emojis = require("../emojis.json")
 
 dotenv.config();
 const TOKEN : string | undefined = process.env.DISCORD_TOKEN;
@@ -14,5 +15,12 @@ const client : Client = new Client({
 client.once('ready', () => {
   console.log(`Connected as ${client.user?.tag}`);
 });
+
+client.on("messageCreate", (message : Message) => {
+  if(message.author.bot) return;
+  let test = emojis[message.cleanContent.toUpperCase()] ? emojis[message.cleanContent.toUpperCase()] : ""
+  if(test === "") return
+  message.channel.send("<:" + message.cleanContent.toUpperCase()+ "_grey:" + test.grey + "> <:" + message.cleanContent.toUpperCase() + "_yellow:" + test.yellow + "> <:" + message.cleanContent.toUpperCase() +  "_green:" + test.green + ">")
+})
 
 client.login(TOKEN).catch(console.error);
